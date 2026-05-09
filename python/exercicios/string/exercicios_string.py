2c2c81bca42a227b8282374235603de5dd5e624d
Flavio Brandão <flavio.brandao58@gmail.com>
2026-05-08 21:55:41 -0300

    exercicios_string.py
    alguns exercicios basicos de formatação de string

---
+++ python/exercicios/string/exercicios_string.py
@@ -0,0 +1,34 @@
+# =========================================
+# DEIXA MAIÚSCULO
+# =========================================
+frase = input("Digite aqui uma frase: ")
+
+print(frase.upper())
+
+# =========================================
+# DEIXA MINUSCULO
+# =========================================
+frase = input("Digite aqui uma frase: ")
+
+print(frase.lower())
+
+# =========================================
+# REMOVE ESPAÇOS
+# =========================================
+frase = input("Digite aqui uma frase: ")
+
+print(frase.strip()) 
+
+# =========================================
+# MAIS DE UMA FORMATAÇÃO
+# =========================================
+frase = input("Digite aqui uma frase: ")
+
+print(frase.strip().lower())
+
+# =========================================
+# TROCA CARACTERE
+# =========================================
+frase = input("Digite aqui uma frase: ")
+
+print(frase.replace("e", "f"))

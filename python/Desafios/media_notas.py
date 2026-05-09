3ddc33775cd9a2b1aa8d7f35a236275993617604
Flavio Brandão <flavio.brandao58@gmail.com>
2026-05-08 22:16:31 -0300

    media_notas.py
    Código que calcula três notas e entrega a media a maior e a menor nota

---
+++ python/Desafios/media_notas.py
@@ -0,0 +1,12 @@
+3 CONSELHO DE NOTAS
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+maior = max(nota1, nota2, nota3)
+menor = min(nota1, nota2, nota3)
+
+print(f"O sua média de nota é: {((nota1 + nota2 + nota3) / 3):.1f}")
+print(f"Sua maior nota é: {maior}:.1f")
+print(f"Sua menor nota é: {menor}:.1f")
